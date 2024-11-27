0c40a85e668831c1767a853772996392c4624917
Xiao-Dree <andrecarlos.oc@gmail.com>
2024-11-27 15:00:50 -0300

    navbar_responsiva and menu_hamburguer

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import pesquisa_icon_dark from '../../assets/search-w.png'
 import button_luz from '../../assets/night.png'
 import button_dark from '../../assets/day.png'
+import menu_aberto from '../../assets/menu_white_36dp.svg'
+import menu_close from '../../assets/close_white_36dp.svg'
+import menu_close_black from '../../assets/close_black.svg'
+import menu_aberto_black from '../../assets/menu_black.svg'
 import "./style.css"
 
 const Navbar = ({tema, setTema}) => {
+// função para mudar o fundo
   const mudar_fundo = () => {
     tema == 'dark' ? setTema('ligth') : setTema('dark');
     
-  }
+  };
+// função para abrir e fechar o menu
+  const [aberto, setAberto] = useState(false);
+  const abrirMenu = () => {
+    setAberto(!aberto);
+  };
+// consigo fechar o menu ao clicar em um item do proprio
+  const fecharMenu = () => {
+    setAberto(false);
+}
+// usar duas fuções ao mesmo tempo de acordo com o tema
+const menuIcon = tema === 'ligth' 
+? (aberto ? menu_close_black : menu_aberto_black) 
+: (aberto ? menu_close : menu_aberto);
+
   return (
     <div className='navbar'>
-      <h1 className='logo'>André <a className='ponto'>.</a></h1>
-      <ul>
+      <h1 className='logo'>André .</h1>
+      <ul className='nav-item'>
         <li>Home</li>
         <li>Serviços</li>
         <li>Resumo</li>
         <li>Projetos</li>
         <li className='form-contato'>Contate-me</li>
       </ul>
-
-      <div className='pesquisa'>
-          <input type="text" placeholder='ser um ótimo dev...' disabled/>
-          <img src={pesquisa_icon_dark} alt="" />
+      <div >
+        <ul onClick={fecharMenu} className={`mobile_menu ${aberto ? 'open' : ''}`}>
+          <li> Home</li>
+          <li>Serviços</li>
+          <li>Resumo</li>
+          <li>Projetos</li>
+          <li className='form-contato'>Contate-me</li>
+        </ul>
       </div>
 
       <img onClick={()=>{mudar_fundo()}} src={tema == 'dark' ? button_dark : button_luz} alt="" className='luz'/>
-
-        <div className='menu_cell'>
-        <ul>
-        <li>Home</li>
-        <li>Serviços</li>
-        <li>Resumo</li>
-        <li>Projetos</li>
-        <li className='form-contato'>Contate-me</li>
-      </ul>
-        </div>
+      <img className='button_menu_cell' onClick={()=>{abrirMenu()}} src={menuIcon} alt="" />
     </div>
     
   )

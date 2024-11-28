1eed293a6e6e46219d80dadeaa404423477e595a
Xiao-Dree <andrecarlos.oc@gmail.com>
2024-11-28 15:10:08 -0300

    home_responsivo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,30 +4,30 @@ import './style.css'
 import whatz from '../../assets/whatsapp.png'
 import github from '../../assets/github.png'
 import linkedin from '../../assets/linkedin.png'
+
 const Home = () => {
   return (
-    <div className='flex'>
-      <div id='container' className='py-24 px-[5%] text-[#ffffff]'>
-            <span className='text-[#767575] text-xl'>Software Developer | Front-end Developer</span>
-            <h1 className='text-[3.5rem] font-bold'>Hello I'm</h1>
-            <h1 className="text-[3.5rem] font-bold text-[#0086B0]">André Carlos</h1>
-          <p className='text-[1rem] text-justify max-w-[35vw]'>I create digital solutions that prioritize both aesthetics and usability. 
-            My portfolio showcases a variety of projects that demonstrate my expertise
-            in diverse technologies and innovative design approaches.</p>
-            <div className='flex gap-10 mt-12'>
-              <a id='cv' className='font-bold border-solid border-2 text-[white] border-[#0086B0] p-2 rounded-2xl bg-[#0086B0] hover:bg-white hover:text-[black]' href="">DOWNLOAD CV</a>
-              <a href="https://www.linkedin.com/in/andr%C3%A9-carloscx/"><img className='w-10 ' src={linkedin} alt="" /></a>
-              <a href="https://github.com/AndreCarlos007"><img className='w-10' src={github} alt="" /></a>
-              <a href=""><img className='w-10' src={whatz} alt="" /></a>
-            </div>
-           
-      </div>
-      <div>
-        <div className='box'>
-        <img id='astro' className='w-[23rem] ml-52 mt-20' src={Astro} alt="" />
+    <div className='flex flex-col-reverse md:flex-row justify-around items-center py-24 px-[5%] '>
+      <div id='container' className='text-[#ffffff] text-center md:text-left md:max-w-[45vw]'>
+        <span className='text-[#767575] text-xl'>Software Developer | Front-end Developer</span>
+        <h1 className='text-[3rem] md:text-[3.5rem] font-bold mt-4'>Hello I'm</h1>
+        <h1 className="text-[3rem] md:text-[3.5rem] font-bold text-[#0086B0]">André Carlos</h1>
+        <p className='text-[1rem] text-justify mt-4 md:max-w-[35vw] mx-auto'>
+          I create digital solutions that prioritize both aesthetics and usability. 
+          My portfolio showcases a variety of projects that demonstrate my expertise
+          in diverse technologies and innovative design approaches.
+        </p>
+        <div className='flex gap-10 mt-12 justify-center md:justify-start'>
+          <a id='cv' className='font-bold border-solid border-2 text-[white] border-[#0086B0] p-2 rounded-2xl bg-[#0086B0] hover:bg-white hover:text-[black]' href="">DOWNLOAD CV</a>
+          <a href="https://www.linkedin.com/in/andr%C3%A9-carloscx/"><img className='w-10' src={linkedin} alt="linkedin" /></a>
+          <a href="https://github.com/AndreCarlos007"><img className='w-10' src={github} alt="github" /></a>
+          <a href=""><img className='w-10' src={whatz} alt="whatsapp" /></a>
         </div>
       </div>
-       
+
+      <div className='flex justify-center items-center mt-8 md:mt-0'>
+        <img id='astro' className='w-[18rem] md:w-[23rem] lg:w-[25rem] ' src={Astro} alt="astro" />
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': '480px',     // Personalizado, pode ser utilizado para telas extras pequenas
+      'sm': '640px',     // Breakpoint padrão para telas pequenas
+      'md': '768px',     // Breakpoint padrão para telas médias
+      'lg': '1024px',    // Breakpoint padrão para telas grandes
+      'xl': '1280px',    // Breakpoint para telas extra grandes
+      '2xl': '1536px',   // Breakpoint para telas maiores
+      '3xl': '1400px',   // Novo breakpoint personalizado até 1400px
+    },
     extend: {},
   },
   plugins: [],

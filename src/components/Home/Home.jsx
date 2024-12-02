906742f02a6a9fe8df9540a0657ca458b54fee0e
Xiao-Dree <andrecarlos.oc@gmail.com>
2024-12-02 12:42:59 -0300

    letreiros_h1_home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,13 +10,15 @@ const Home = () => {
     <div className='flex flex-col-reverse md:flex-row justify-between items-center pb-[6rem] px-[5%] w-[100vw] md:h-[calc(100vh-100px)] md:pt-[100px] xl:h-[calc(100vh-121px)]'>
       <div id='container' className='text-[#ffffff] text-center md:text-left md:max-w-[45vw]'>
         <span className='text-[#767575] text-xl'>Software Developer | Front-end Developer</span>
-        <h1 className='text-[3rem] md:text-[3.5rem] font-bold'>Hello I'm</h1>
-        <h1 className="text-[3.2rem] md:text-[3.5rem] font-bold text-[#0086B0]">André Carlos</h1>
+        <h1 id='ola' className='text-[3rem] md:text-[3.5rem] font-bold'>Hello I'm</h1>
+        <h1 id='nome' className="text-[3rem] md:text-[3.5rem] font-bold text-[#0086B0]">André Carlos</h1>
+        <div>
         <p className='text-[1rem] text-justify mt-4 md:max-w-[35vw] mx-auto'>
           I create digital solutions that prioritize both aesthetics and usability. 
           My portfolio showcases a variety of projects that demonstrate my expertise
           in diverse technologies and innovative design approaches.
         </p>
+        </div>
         <div className='flex gap-10 mt-12 justify-center md:justify-start'>
           <a id='cv' className='font-bold border-solid border-2 text-[white] border-[#0086B0] p-2 rounded-2xl bg-[#0086B0] hover:bg-white hover:text-[black]' href="">CURRICULUM</a>
           <a href="https://www.linkedin.com/in/andr%C3%A9-carloscx/"><img className='w-10' src={linkedin} alt="linkedin" /></a>
